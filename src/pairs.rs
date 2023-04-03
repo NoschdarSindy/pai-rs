e132484073c8c8f3c4a56ba53adc01218b55407d
Paul Kantiem <paul@kantiem.de>
2023-04-03 16:30:48 +0200

    fix

--- src/pairs.rs
+++ src/pairs.rs
@@ -4,13 +4,12 @@ use std::fmt::Display;
 
 type Vec2D<T> = Vec<Vec<T>>;
 
-
 #[derive(Clone, Debug)]
-struct Vec2<T> { 
-    x: T, y: T 
+struct Vec2<T> {
+    x: T,
+    y: T,
 }
 type Position = Vec2<usize>;
-
 
 #[derive(Clone, Debug)]
 struct Field {
@@ -23,23 +22,29 @@ pub struct Pairs {
     cols: usize,
     rows: usize,
     open: (Option<Position>, Option<Position>),
-    field: Vec2D<Option<Field>>
+    field: Vec2D<Option<Field>>,
 }
 
 impl Pairs {
-
     pub fn new(cols: usize, rows: usize) -> Self {
         if (cols * rows) % 2 != 0 {
             panic!("Even number of cards is needed!");
         }
-        Self { cols, rows, open: (None, None), field: Pairs::init_random_field(cols, rows) }
+        Self {
+            cols,
+            rows,
+            open: (None, None),
+            field: Pairs::init_random_field(cols, rows),
+        }
     }
-
 
     fn init_random_field(cols: usize, rows: usize) -> Vec2D<Option<Field>> {
         let mut pairs = Vec::new();
         for i in 0..((cols * rows) / 2) {
-            let field = Field { symbol: i, open: false };
+            let field = Field {
+                symbol: i,
+                open: false,
+            };
             pairs.push(field.clone());
             pairs.push(field);
         }
@@ -53,7 +58,6 @@ impl Pairs {
     }
 
     pub fn open(&mut self, x: usize, y: usize) {
-
         if let None = self.field[x][y] {
             return;
         }
@@ -64,21 +68,26 @@ impl Pairs {
                 self.field[x][y].as_mut().unwrap().open = true;
                 self.open.0 = Some(Position { x, y });
             }
+
             // Bisher ist nur eine Karte aufgedeckt
             (Some(first), None) => {
+                // Die gleiche Karte wurde ausgewählt
+                if first.x == x && first.y == y {
+                    return;
+                }
+                // Wurde ein Paar gefunden?
                 let first_symbol = self.field[first.x][first.y].as_ref().unwrap().symbol;
                 let second_symbol = self.field[x][y].as_ref().unwrap().symbol;
-
-                // Wurde ein Paar gefunden?
                 if first_symbol == second_symbol {
                     self.field[first.x][first.y] = None;
                     self.field[x][y] = None;
                     self.open = (None, None);
                 } else {
                     self.field[x][y].as_mut().unwrap().open = true;
-                    self.open.1 = Some(Position {x, y });
+                    self.open.1 = Some(Position { x, y });
                 }
             }
+
             // Es sind bereits zwei Karten aufgedeckt
             (Some(first), Some(second)) => {
                 // Die alten Karten wieder verdecken
@@ -88,12 +97,10 @@ impl Pairs {
                 self.field[x][y].as_mut().unwrap().open = true;
                 self.open = (Some(Position { x, y }), None);
             }
-            _ => {},
+            _ => {}
         }
     }
-
 }
-
 
 impl Display for Pairs {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -105,7 +112,9 @@ impl Display for Pairs {
                             let symbol = char::from_u32(128053 + field.symbol as u32).unwrap();
                             f.write_str(&format!("{} ", symbol));
                         }
-                        false => { f.write_str("🟪 "); }
+                        false => {
+                            f.write_str("🟪 ");
+                        }
                     }
                 } else {
                     f.write_str("⬜ ");
@@ -116,7 +125,6 @@ impl Display for Pairs {
         Ok(())
     }
 }
-
 
 #[cfg(test)]
 mod tests {
